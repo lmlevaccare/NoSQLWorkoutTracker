7cbb9edd80096d4d15281f6ace33d287179c8ad1
lauren levaccare <laurenlevaccare@icloud.com>
2021-02-08 12:10:19 -0600

    server.js file running

---
+++ Develop/public/api.js
@@ -0,0 +1,46 @@
+// get routes to view lastworkout api connecting to font end html
+const API = {
+  async getLastWorkout() {
+    let res;
+    try {
+      res = await fetch("/api/workouts");
+    } catch (err) {
+      console.log(err)
+    }
+    const json = await res.json();
+
+    return json[json.length - 1];
+  },
+  // put route
+  async addExercise(data) {
+    const id = location.search.split("=")[1];
+
+    const res = await fetch("/api/workouts/" + id, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data)
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+  async createWorkout(data = {}) {
+    const res = await fetch("/api/workouts", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    const json = await res.json();
+
+    return json;
+  },
+
+  async getWorkoutsInRange() {
+    const res = await fetch(`/api/workouts/range`);
+    const json = await res.json();
+
+    return json;
+  },
+};
---
+++ Develop/server.js
@@ -0,0 +1,31 @@
+
+
+
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+const models = require("./models");
+
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+require("./routes/api_routes")(app);
+require("./routes/html_routes")(app);
+
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
